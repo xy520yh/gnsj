bd8a13d396a7f971e5da0659cba41bf326d5974c
unknown <705719648@qq.com>
2023-06-20 17:07:09 +0800

    2

--- js/qiehuan.js
+++ js/qiehuan.js
@@ -13,46 +13,10 @@ window.onresize = function(){
   rePosition();
 };          
 
-$(function () {
- /*part8--花灵*/ 
-  var flowerswiper = new Swiper('.part8_swiper',{
-      loop:true, initialSlide: 8,
-      autopaly:true,
-      slideToClickedSlide: true,
-      slidesPerView: 4,
-      freeMode: true,watchSlidesProgress: true,
-      navigation: {
-        nextEl: '.part8_menu  .swiper-button-next',
-        prevEl: '.part8_menu .swiper-button-prev',
-  },
-  on: {
-    resize: function () {
-            setTimeout(function(){
-              flowerswiper.update()
-            },500)
-            
-      },
-    slideChangeTransitionEnd:function(flowerswiper){ 
-          var index = this.realIndex;
-            //role.slideTo(swiper1.realIndex);
-          $('.role .content_tro dd').eq(index).fadeIn().siblings().hide();
-            var dd = $('.role .content_tro dd').eq(index);
-          //  console.log(dd.length)
-              $('.role .content_tro dd').hide()
-              dd.show();
-            //  dd.find('.role_name').addClass('animated fadeInDown');
-            //  dd.find('.role_people').addClass('animated fadeInUp');
-            //  dd.find('.note').addClass('animated fadeInRight');
-          },
-        },
-  });
-
-      
-});
  
 $(function () {
 
-	// 首屏社区qq
+	  // 首屏社区qq
   $('.part1_qq').click(function(){
     $('.part1_qq_code').fadeToggle();
    $('.part1_wx_code').css('display','none');
@@ -62,7 +26,7 @@ $(function () {
    $('.part1_qq_code').css('display','none');
   })
 
-	// 首屏点击“立刻启程“展开预约方式
+	// 点击“立刻启程“展开预约方式
   $('.part_btn_cbt').click(function(){
     $(' .masked').css('display','block');
     $('.tc_way').css('display','block');
@@ -72,20 +36,6 @@ $(function () {
       $('.masked').css('display','none');
   })
 
-   // 首屏视频弹窗弹出
-   $(document).ready(function(){
-    $("#playbtn").colorbox({iframe:true, innerWidth:"50%", innerHeight:"60%"});
-  }) ; 
-   	// 首屏点击预约弹窗“立即预约“按钮弹出分享成功
-    $('.way_btn').click(function(){
-      $(' .masked').css('display','block');
-      $('.tc_yy_sucess').css('display','block');
-      $('.tc_way').css('display','none');
-    })
-    $('.tc_yy_sucess .closed').click(function(){
-      $(this).parent().parent().css('display','none');
-      $('.masked').css('display','none');
-    })
 
     // 点击活动规则
     $('.part_box_rule').click(function(){
@@ -107,30 +57,30 @@ $(function () {
         $('.masked').css('display','none');
       })
 
-
-
-  
-
-      // 第四屏点击抽奖按钮
-        $('.part_box_btn').click(function(){
-          $(' .masked').css('display','block');
-          $('.tc_goddess_box').css('display','block');
-        })
-        $('.tc_goddess_box .closed').click(function(){
-          $('.tc_goddess_box').css('display','none');
-          $('.masked').css('display','none');
-        })
-
-          // 第四屏版权展开收起
-    $('.part3_bless_click').click(function(){
+    // 点击领取
+    $('.part_box_btn').click(function(){
       $(' .masked').css('display','block');
-      $('.part3_bless_tc').css('display','block');
+      $('.tc_goddess_box').css('display','block');
     })
-    $('.part3_bless_tc .closed_bless').click(function(){
-       $('.part3_bless_tc').css('display','none');
-        $('.masked').css('display','none');
+    $('.tc_goddess_box .closed').click(function(){
+      $('.tc_goddess_box').css('display','none');
+      $('.masked').css('display','none');
     })
 
+       // 点击祝福语
+         $('.part3_bless_click').click(function(){
+          $(' .masked').css('display','block');
+          $('.part3_bless_tc').css('display','block');
+        })
+        $('.part3_bless_tc .closed_bless').click(function(){
+           $('.part3_bless_tc').css('display','none');
+            $('.masked').css('display','none');
+        })
+    
+
+     
+
+   
   	// 注册框里面获取焦点
 	//  $(".text_content1").focus(function(){
 	// 	if($(this).val()=='请输入电话)'){
